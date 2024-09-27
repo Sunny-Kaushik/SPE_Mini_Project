3656be6855fcc0b33e887857c6d2723e69d747d2
Sunny Kaushik <sunny.kaushik2305@gmail.com>
2024-09-27 11:56:19 +0530

    testing

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -17,7 +17,7 @@ public class Calculator {
         if (x < 0) {
             throw new IllegalArgumentException("Factorial of a negative number is not defined.");
         }
-        long result = 2;
+        long result = 1;
         for (int i = 1; i <= x; i++) {
             result *= i;
         }
